16fe1b2c51dd3011909158af58e684c84cc81405
Kraudia <klaudienne@gmail.com>
2017-05-08 23:58:24 +0200

    menu and search input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'filmy',
+    component: MoviesComponent,
+    data: { title: 'Allegro' }
+  },
+  { path: '',
+    redirectTo: '/filmy',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
