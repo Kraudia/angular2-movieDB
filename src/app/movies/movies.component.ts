42a545a847e8bc82edbf89096b89dab572ee3848
Kraudia <klaudienne@gmail.com>
2017-05-14 19:53:25 +0200

    sort by title or popularity

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -4,9 +4,6 @@ import { Observable }               from 'rxjs/Observable';
 import { MoviesService }            from './movies.service';
 import { Movie }                    from './movie';
 
-//jQuery
-declare var $: any;
-
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
@@ -16,6 +13,7 @@ declare var $: any;
 export class MoviesComponent implements OnInit {
   movies: Observable<Movie[]>;
   language: string;
+  sort: number;
 
   constructor(
     private moviesService: MoviesService,
@@ -29,6 +27,41 @@ export class MoviesComponent implements OnInit {
 
   getMovies() {
     this.movies = this.moviesService.getMovies();
+  }
+
+  dynamicSort(property) {
+    let sortOrder = 1;
+    if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function (a,b) {
+      let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+      return result * sortOrder;
+    }
+  }
+
+  sortMovies(property: string) {
+    if (property == 'title') {
+      if (this.sort == 1) {
+        this.movies = this.movies.map(items => items.sort(this.dynamicSort("-title")));
+        this.sort = -1;
+      }
+      else {
+        this.movies = this.movies.map(items => items.sort(this.dynamicSort("title")));
+        this.sort = 1;
+      }
+    }
+    else if (property == 'popularity') {
+      if (this.sort == 2) {
+        this.movies = this.movies.map(items => items.sort(this.dynamicSort("-popularity")));
+        this.sort = -2;
+      }
+      else {
+        this.movies = this.movies.map(items => items.sort(this.dynamicSort("popularity")));
+        this.sort = 2;
+      }
+    }
   }
 
   onSelect(movie: Movie) {
--- src/app/search/search-movies.component.ts
+++ src/app/search/search-movies.component.ts
@@ -12,8 +12,10 @@ import { Search }                    from '../movies/search';
 })
 export class SearchMoviesComponent implements OnInit {
   searches: Search;
+  movies: Movie[];
   query: string;
   language: string;
+  sort: number;
 
   constructor(
     private searchService: SearchService,
@@ -35,11 +37,46 @@ export class SearchMoviesComponent implements OnInit {
     this.searchService.searchMovies(query)
       .subscribe(
         response => {
-          this.searches = response['results'];
-          console.log("res", response);
+          this.searches = response;
+          this.movies = response['results'];
         },
         error => console.error(error)
       );
+  }
+
+  dynamicSort(property) {
+    let sortOrder = 1;
+    if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+    return function (a,b) {
+      let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+      return result * sortOrder;
+    }
+  }
+
+  sortMovies(property: string) {
+    if (property == 'title') {
+      if (this.sort == 1) {
+        this.movies.sort(this.dynamicSort("-title"));
+        this.sort = -1;
+      }
+      else {
+        this.movies.sort(this.dynamicSort("title"));
+        this.sort = 1;
+      }
+    }
+    else if (property == 'popularity') {
+      if (this.sort == 2) {
+        this.movies.sort(this.dynamicSort("-popularity"));
+        this.sort = -2;
+      }
+      else {
+        this.movies.sort(this.dynamicSort("popularity"));
+        this.sort = 2;
+      }
+    }
   }
 
   onSelect(movie: Movie) {
